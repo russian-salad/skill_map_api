82db29840107a88eeb4dd1458313843e1b933a03
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-06-01 23:23:00 +0300

    добавил запрос для получения специализации по name; добавил запрос для добавления новой специализации в бд

--- app/database/query.py
+++ app/database/query.py
@@ -1,10 +1,21 @@
 from sqlalchemy.orm import Session
 
 from database.models import specializations
-
+from schemas.specializations import SpecializationBase
 
 def get_all_specializations(session: Session):
     return session.query(specializations.Specialization).filter().all()
 
-def get_one_specializations(session: Session, id: int):
+def get_one_specialization(session: Session, id: int):
     return session.query(specializations.Specialization).filter(specializations.Specialization.id == id).first()
+
+def get_specialization_by_name(session: Session, name: str):
+    return session.query(specializations.Specialization).filter(specializations.Specialization.name == name).first()   
+
+def create_specialization(session: Session, specialization: SpecializationBase ):
+    new_specialization = specializations.Specialization(name=specialization.name,  description=specialization.description)
+    
+    session.add(new_specialization)
+    session.commit()
+    session.refresh(new_specialization)
+    return new_specialization
