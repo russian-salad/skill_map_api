bbb81050545e626a429f69f3120faeb74c0ec92f
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-06-01 23:24:29 +0300

    добавил роутер для создания специализации

--- app/routers/specializations.py
+++ app/routers/specializations.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -11,11 +11,19 @@ from dependencies.db import get_db
 router = APIRouter()
 
 
-@router.get('/all_specialisation', response_model=List[SpecializationBase])
-async def get_all_specialisation(session: Session = Depends(get_db)):
+@router.get('/all_specializations', response_model=List[SpecializationBase])
+async def get_all_specializations(session: Session = Depends(get_db)):
     return query.get_all_specializations(session)
 
 
-@router.get('/all_specialisation/{id}', response_model=SpecializationBase)
-async def get_one_specialisation(id: int,session: Session = Depends(get_db)):
-    return query.get_one_specializations(session, id)
+@router.get('/all_specialization/{id}', response_model=SpecializationBase)
+async def get_one_specialization(id: int,session: Session = Depends(get_db)):
+    return query.get_one_specialization(session, id)
+
+
+@router.post('/add_specialization', response_model=SpecializationBase)
+async def create_specialization(specialization: SpecializationBase, session: Session = Depends(get_db)):
+    current_specialization = query.get_specialization_by_name(session, name=specialization.name)
+    if current_specialization:
+        raise HTTPException(status_code=400, detail="Specialization already exists")
+    return query.create_specialization(session, specialization)
