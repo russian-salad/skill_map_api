bb816022d22c45f9b5d34b222357270f0d2905d4
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-06-01 19:51:07 +0300

    добавил запрос для получения одной специализации

--- app/database/query.py
+++ app/database/query.py
@@ -4,5 +4,7 @@ from database.models import specializations
 
 
 def get_all_specializations(session: Session):
-    
     return session.query(specializations.Specialization).filter().all()
+
+def get_one_specializations(session: Session, id: int):
+    return session.query(specializations.Specialization).filter(specializations.Specialization.id == id).first()
