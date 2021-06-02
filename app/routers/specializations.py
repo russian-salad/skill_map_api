f4d7ca67be8f8d6b58e6fb97e65b95ac85a31763
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-06-02 19:21:26 +0300

    добавли роутер для удаление специализации

--- app/routers/specializations.py
+++ app/routers/specializations.py
@@ -26,4 +26,15 @@ async def create_specialization(specialization: SpecializationBase, session: Ses
     current_specialization = query.get_specialization_by_name(session, name=specialization.name)
     if current_specialization:
         raise HTTPException(status_code=400, detail="Specialization already exists")
-    return query.create_specialization(session, specialization )
+    return query.create_specialization(session, specialization)
+
+
+@router.delete('/delete_specialization', response_model=SpecializationBase)
+async def delete_specialization(specialization: SpecializationBase, session: Session = Depends(get_db)):
+    print(specialization.name)
+    current_specialization = query.get_specialization_by_name(session, name=specialization.name)
+    if current_specialization:
+        return query.delete_specialization(session, name = specialization.name)
+    else: 
+        raise HTTPException(status_code=400, detail="Specialization does not exist")
+    
