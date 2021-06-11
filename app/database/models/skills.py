f0470ee6b0d50f94b657afd95e2b9ab03cf51f1f
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-06-11 10:32:07 +0300

    добавил создание связанной таблицы

--- app/database/models/skills.py
+++ app/database/models/skills.py
@@ -1,8 +1,14 @@
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import Column, Integer, String, Table
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
 
 from database import Base
+from . import sources
 
+association_table_skill_to_source = Table('association_table_skill_to_source', Base.metadata,
+    Column('skill_id', Integer, ForeignKey('skill.id')),
+    Column('source_id', Integer, ForeignKey('source.id'))
+)
 class Skill(Base):
     '''
     Database model Skill
@@ -12,3 +18,4 @@ class Skill(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     description = Column(String)
+    source = relationship('Source', secondary=association_table_skill_to_source)
