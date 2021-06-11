973e4bfdb6cf3141feb19c790743670cd56db945
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-06-11 10:32:31 +0300

    добавил связанную таблицу

--- app/database/models/specializations.py
+++ app/database/models/specializations.py
@@ -1,6 +1,14 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Table
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
 
 from database import Base
+
+
+association_table_specialization_to_skills = Table('association_table_specialization_to_skills', Base.metadata,
+    Column('specialization_id', Integer, ForeignKey('specialization.id')),
+    Column('skill_id', Integer, ForeignKey('skill.id'))
+)
 
 class Specialization(Base):
     '''
@@ -11,3 +19,4 @@ class Specialization(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     description = Column(String)
+    skill = relationship("Skill", secondary=association_table_specialization_to_skills)
